0b93527ce003c6fb6bd7feac14339f8f43ca93b0
Roan Hofland <roan@roanh.dev>
2021-10-05 16:07:04 +0200

    A lot of graph testing and prototyping

---
+++ Testing/src/GraphTest.java
@@ -0,0 +1,222 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class GraphTest{
+
+	public static void main(String[] args){
+		Graph graph = new Graph();
+		for(int i = 1; i <= 18; i++){
+			graph.addNode(String.valueOf(i));
+		}
+		
+		graph.addEdge(1, 2, "a");
+		graph.addEdge(1, 4, "b");
+		graph.addEdge(4, 6, "c");
+		graph.addEdge(4, 5, "d");
+		graph.addEdge(5, 6, "y");
+		graph.addEdge(6, 5, "e");
+		graph.addEdge(2, 5, "f");
+		graph.addEdge(2, 3, "g");
+		graph.addEdge(5, 7, "h");
+		graph.addEdge(3, 7, "i");
+		graph.addEdge(7, 9, "j");
+		graph.addEdge(6, 8, "k");
+		graph.addEdge(9, 12, "l");
+		graph.addEdge(8, 12, "m");
+		graph.addEdge(8, 11, "n");
+		graph.addEdge(11, 14, "o");
+		graph.addEdge(14, 12, "p");
+		graph.addEdge(10, 7, "q");
+		graph.addEdge(13, 10, "r");
+		graph.addEdge(16, 13, "s");
+		graph.addEdge(14, 16, "t");
+		graph.addEdge(14, 15, "u");
+		graph.addEdge(15, 18, "v");
+		graph.addEdge(18, 17, "w");
+		graph.addEdge(17, 16, "x");
+		
+//		for(Edge e : edges){
+//			System.out.println(e.source.id + " " + e.name + "-> " + e.target.id);
+//		}
+		
+		Edge[] hist = new Edge[6];
+		List<Edge[]> paths = new ArrayList<Edge[]>();
+		printPaths("", graph.nodeMap.get("1"), graph.nodeMap.get("12"), 6, hist, paths);
+		
+		for(Edge[] p : paths){
+			System.out.println(Arrays.toString(p));
+		}
+		
+		//make the edge graph
+		Graph edgeGraph = new Graph();
+		edgeGraph.addNode("src");
+		edgeGraph.addNode("trg");
+		graph.edges.forEach(e->edgeGraph.addNode(e.name));
+		
+		for(Edge[] p : paths){
+			edgeGraph.addEdgeIfNotExists("src", p[0].name);
+			for(int i = 0; i < p.length - 1; i++){
+				edgeGraph.addEdgeIfNotExists(p[i].name, p[i + 1].name);
+			}
+			edgeGraph.addEdgeIfNotExists(p[p.length - 1].name, "trg");
+		}
+		
+		printParallel(edgeGraph);
+	}
+	
+	public static void printParallel(Graph g){
+		Node source = g.nodeMap.get("src");
+		
+		Deque<Node> head = new ArrayDeque<Node>();
+		head.push(source);
+		//store predecessor node
+		Map<Node, Node> pred = new HashMap<Node, Node>();
+		//mark processed nodes
+		Set<Node> found = new HashSet<Node>();
+		
+		List<Runnable> reverseTasks = new ArrayList<Runnable>();
+		
+		while(!head.isEmpty()){
+			Node n = head.pop();//random?
+			System.out.println("Handling: " + n.name);
+			for(Edge e : n.out){//random?
+				System.out.println("out edge to: " + e.target.name);
+				if(!found.contains(e.target)){
+					pred.put(e.target, n);
+					found.add(e.target);
+					head.push(e.target);
+				}else{
+					reverseTasks.add(()->reverseTask(n, pred, source, e));
+				}
+			}
+		}
+		
+		reverseTasks.forEach(Runnable::run);
+	}
+	
+	private static void reverseTask(Node n, Map<Node, Node> pred, Node source, Edge e){
+		//cycle it, we basically reverse the route how we got here
+		List<Node> secondPath = new ArrayList<Node>();
+		{
+			Node prev = n;
+			do{
+				secondPath.add(prev);
+				prev = pred.get(prev);
+			}while(prev != source);
+		}
+		Collections.reverse(secondPath);//since we added nodes from the end of the path first
+		
+		//if the current node has multiple back links then those are all 'cycles'
+		for(Edge back : e.target.in){
+			if(back != e){//that's the way we came...
+				//System.out.println("follow back to: " + back.source + " | " + e + " | " + back);
+				List<Node> firstPath = new ArrayList<Node>();
+				Node prev = back.source;
+				do{
+					//System.out.println("back: " + prev);
+					firstPath.add(prev);
+					prev = pred.get(prev);
+					//if(prev == null){
+					//	throw new RuntimeException("back for " + prev + " is null");
+					//}
+				}while(prev != source);
+				Collections.reverse(firstPath);
+				
+				//it's entirely possible we went back too far and that both paths have an identical prefix
+				int offset = 0;
+				while(firstPath.get(offset) == secondPath.get(offset)){
+					offset++;
+				}
+				
+				System.out.println("parallel: " + firstPath.subList(offset, firstPath.size()) + " | " + secondPath.subList(offset, secondPath.size()));
+			}
+		}
+	}
+	
+	public static void printPaths(String path, Node source, Node target, int maxLen, Edge[] hist, List<Edge[]> paths){
+		if(source == target){
+			System.out.println("path: " + path);
+			paths.add(Arrays.copyOf(hist, hist.length - maxLen));
+		}else if(maxLen == 0 || source.out.isEmpty()){
+			return;
+		}else{
+			for(Edge edge : source.out){
+				hist[hist.length - maxLen] = edge;
+				printPaths(path + edge.name, edge.target, target, maxLen - 1, hist, paths);
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	private static final class Graph{
+		private List<Edge> edges = new ArrayList<Edge>();
+		private List<Node> nodes = new ArrayList<Node>();
+		private Map<String, Node> nodeMap = new HashMap<String, Node>();
+		
+		private void addEdge(String source, String target, String name){
+			edges.add(new Edge(nodeMap.get(source), nodeMap.get(target), name));
+		}
+		
+		private void addEdge(int source, int target, String name){
+			addEdge(String.valueOf(source), String.valueOf(target), name);
+		}
+		
+		private void addEdgeIfNotExists(String source, String target){
+			if(nodeMap.get(source).out.stream().noneMatch(e->e.target.name.equals(target))){
+				addEdge(source, target, null);
+			}
+		}
+		
+		private void addNode(String id){
+			Node n = new Node(id);
+			nodes.add(n);
+			nodeMap.put(id, n);
+		}
+	}
+	
+	private static final class Edge{
+		Node source;
+		Node target;
+		String name;
+		
+		private Edge(Node source, Node target, String name){
+			this.source = source;
+			this.target = target;
+			this.name = name;
+			source.out.add(this);
+			target.in.add(this);
+		}
+		
+		@Override
+		public String toString(){
+			return "(" + source + "," + name + "," + target + ")";
+		}
+	}
+	
+	private static final class Node{
+		String name;
+		List<Edge> out = new ArrayList<Edge>();
+		List<Edge> in = new ArrayList<Edge>();
+		
+		private Node(String id){
+			this.name = id;
+		}
+		
+		@Override
+		public String toString(){
+			return name;
+		}
+	}
+}

ea1004c0e6c0786553a2cd7f5548d446e916108c
Roan Hofland <roan@roanh.dev>
2021-10-19 18:55:09 +0200

    Partial javadoc for the Graph class

--- gMark/src/dev/roanh/gmark/util/Graph.java
+++ gMark/src/dev/roanh/gmark/util/Graph.java
@@ -30,38 +30,66 @@ public class Graph<V, E>{
 	 * store the same data.
 	 */
 	private Map<V, GraphNode<V, E>> nodeMap = new HashMap<V, GraphNode<V, E>>();
+	/**
+	 * List of all nodes in this graph.
+	 */
 	private List<GraphNode<V, E>> nodes = new ArrayList<GraphNode<V, E>>();
+	/**
+	 * List of all edges in this graph.
+	 */
 	private List<GraphEdge<V, E>> edges = new ArrayList<GraphEdge<V, E>>();
 	
+	/**
+	 * Gets a list of all nodes in this graph.
+	 * @return All nodes in this graph.
+	 */
 	public List<GraphNode<V, E>> getNodes(){
 		return nodes;
 	}
 	
+	/**
+	 * Gets a list of all edges in this graph.
+	 * @return All edges in this graph.
+	 */
 	public List<GraphEdge<V, E>> getEdges(){
 		return edges;
 	}
 	
+	/**
+	 * Gets a single node from this graph by the
+	 * data that is stored at the node to find.
+	 * Note that nodes are assumed to store information
+	 * that uniquely identifies them, meaning the
+	 * given data uniquely identifies a single node.
+	 * @param data The data stored at the node to find.
+	 * @return The node that stores the given data or
+	 *         <code>null</code> if no such node exists.
+	 */
 	public GraphNode<V, E> getNode(V data){
 		return nodeMap.get(data);
 	}
 	
-	//TODO assumed to be unique but not checked
-	//therefore data is also assumed to be unique
+	/**
+	 * Adds a new unique node to this graph that is associated
+	 * with the given data. The data given is assumed to uniquely
+	 * identify a single node in the whole graph. If this graph
+	 * already contains a node associated with the same data then
+	 * a new node is not added to the graph and the existing node
+	 * is returned instead.
+	 * @param data The data to associated with the node.
+	 * @return The (newly added) node that is assocaited
+	 *         with the given data.
+	 */
 	public GraphNode<V, E> addUniqueNode(V data){
-		GraphNode<V, E> node = new GraphNode<V, E>(this, data);
-		
 		GraphNode<V, E> existing = nodeMap.get(data);
 		if(existing != null){
 			return existing;
+		}else{
+			GraphNode<V, E> node = new GraphNode<V, E>(this, data);
+			nodeMap.put(data, node);
+			nodes.add(node);
+			return node;
 		}
-		//should take care of everything
-		
-		if(nodeMap.put(data, node) != null){
-			//TODO
-			throw new IllegalStateException("TODO");
-		}
-		nodes.add(node);
-		return node;
 	}
 	
 	public void addUniqueEdge(V source, V target, E data){
@@ -102,12 +130,40 @@ public class Graph<V, E>{
 		edges.add(new GraphEdge<V, E>(source, target, data));
 	}
 	
+	/**
+	 * Represents a single node in the graph that is
+	 * uniquely identifiable by some data.
+	 * @author Roan
+	 * @param <V> The type of data stored at the graph nodes.
+	 * @param <E> The type of data stored at the graph edges.
+	 */
 	public static class GraphNode<V, E>{
+		/**
+		 * The graph this node is in.
+		 */
 		private Graph<V, E> graph;
+		/**
+		 * The edges departing from this node.
+		 */
 		private Set<GraphEdge<V, E>> out = new HashSet<GraphEdge<V, E>>();
+		/**
+		 * The edges ending at this node.
+		 */
 		private Set<GraphEdge<V, E>> in = new HashSet<GraphEdge<V, E>>();
+		/**
+		 * The data stored at this node, this also
+		 * uniquely identifies this node in the entire graph.
+		 */
 		private V data;
 		
+		/**
+		 * Constructs a new graph node for the given graph
+		 * and with the given data that uniquely identifies
+		 * this node in the entire graph.
+		 * @param graph The graph this node belongs to.
+		 * @param data The data associated with this
+		 *        node that uniquely identifies it in the graph.
+		 */
 		private GraphNode(Graph<V, E> graph, V data){
 			this.graph = graph;
 			this.data = data;
@@ -153,6 +209,12 @@ public class Graph<V, E>{
 			graph.addUniqueEdge(this, target);
 		}
 		
+		/**
+		 * Gets the data stored at this node. The
+		 * returned data uniquely identifies this
+		 * node in the entire graph.
+		 * @return The data stored at this node.
+		 */
 		public V getData(){
 			return data;
 		}
@@ -173,11 +235,35 @@ public class Graph<V, E>{
 		}
 	}
 	
+	/**
+	 * Represents a single directed edge in the graph that
+	 * can have associated data.
+	 * @author Roan
+	 * @param <V> The type of data stored at the graph nodes.
+	 * @param <E> The type of data stored at the graph edges.
+	 */
 	public static class GraphEdge<V, E>{
+		/**
+		 * The source node for this edge.
+		 */
 		private GraphNode<V, E> source;
+		/**
+		 * The target node for this edge.
+		 */
 		private GraphNode<V, E> target;
+		/**
+		 * The data associated with this edge, can be <code>null</code>.
+		 */
 		private E data;
 		
+		/**
+		 * Constructs a new edge with the given source,
+		 * target and associated data.
+		 * @param source The source node for this edge.
+		 * @param target The target node for this edge.
+		 * @param data The data associated with this edge,
+		 *        allowed to be <code>null</code>.
+		 */
 		private GraphEdge(GraphNode<V, E> source, GraphNode<V, E> target, E data){
 			this.source = source;
 			this.target = target;
@@ -186,36 +272,61 @@ public class Graph<V, E>{
 			target.in.add(this);
 		}
 		
+		/**
+		 * Gets the source node for this edge.
+		 * @return The source node for this edge.
+		 * @see #getSource()
+		 */
 		public GraphNode<V, E> getSourceNode(){
 			return source;
 		}
 		
+		/**
+		 * Gets the target node for this edge.
+		 * @return The target node for this edge.
+		 * @see #getTarget()
+		 */
 		public GraphNode<V, E> getTargetNode(){
 			return target;
 		}
 		
+		/**
+		 * Gets the data stored at the source node for this edge.
+		 * @return The data stored at the source node for this edge.
+		 * @see #getSourceNode()
+		 */
 		public V getSource(){
 			return source.data;
 		}
 		
+		/**
+		 * Gets the data stored at the target node for this edge.
+		 * @return The data stored at the target node for this edge.
+		 * @see #getTargetNode()
+		 */
 		public V getTarget(){
 			return target.data;
 		}
 		
+		/**
+		 * Gets the data associated with this edge.
+		 * @return The data associated with this edge,
+		 *         possibly <code>null</code>.
+		 */
 		public E getData(){
 			return data;
 		}
 		
 		@Override
 		public String toString(){
-			return data == null ? "GraphEdge" : data.toString();
+			return data == null ? "GraphEdge[data=null]" : data.toString();
 		}
 		
 		@Override
 		public boolean equals(Object other){
 			if(other instanceof GraphEdge){
 				GraphEdge<?, ?> edge = (GraphEdge<?, ?>)other;
-				return edge.source.equals(source) && edge.target.equals(target);
+				return edge.source.equals(source) && edge.target.equals(target) && edge.data.equals(data);
 			}else{
 				return false;
 			}
@@ -223,7 +334,7 @@ public class Graph<V, E>{
 		
 		@Override
 		public int hashCode(){
-			return Objects.hash(source, target);
+			return Objects.hash(source, target, data);
 		}
 	}
 }

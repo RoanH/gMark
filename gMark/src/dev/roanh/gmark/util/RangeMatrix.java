930027c647db56b6edbb902e917d42a88307199f
Roan Hofland <roan@roanh.dev>
2021-11-07 17:04:34 +0100

    Selectivity graph implementation

--- gMark/src/dev/roanh/gmark/util/RangeMatrix.java
+++ gMark/src/dev/roanh/gmark/util/RangeMatrix.java
@@ -3,34 +3,33 @@ package dev.roanh.gmark.util;
 import java.util.function.Supplier;
 
 public class RangeMatrix<T>{
-	protected int size;
+	private T[][] data;
+	private int size;
 	
 	//square with element init
+	@SuppressWarnings("unchecked")
 	public RangeMatrix(int size, Supplier<T> init){
-		
+		data = (T[][])new Object[size];
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				data[i][j] = init.get();
+			}
+		}
 	}
-	
-	public int getSize(){
-		return -1;//TODO
-	}
-	
-	
-	
-	
-	
-	
-	
 	
 	public T get(IDable row, IDable col){
 		return get(row.getID(), col.getID());
 	}
 	
 	public T get(int row, int col){
-		//TODO
-		return null;
+		return data[row][col];
 	}
 	
 	public void set(int row, int col, T value){
-		
+		data[row][col] = value;
+	}
+	
+	public int getSize(){
+		return size;
 	}
 }
--- gMark/src/dev/roanh/gmark/util/SelectivityGraph.java
+++ gMark/src/dev/roanh/gmark/util/SelectivityGraph.java
@@ -6,14 +6,39 @@ import java.util.Map.Entry;
 import dev.roanh.gmark.core.SelectivityClass;
 import dev.roanh.gmark.core.graph.Edge;
 import dev.roanh.gmark.core.graph.Schema;
+import dev.roanh.gmark.core.graph.Type;
 
-public class SelectivityGraph{
-
+public class SelectivityGraph extends Graph<SelectivityType, Void>{
+	//TODO, move to constructor if not used anywhere else
+	private RangeList<Map<SelectivityClass, GraphNode<SelectivityType, Void>>> index;
 	
 	
-	public SelectivityGraph(){
+	public SelectivityGraph(Schema schema, int maxLength){
+		
 		//compute distance between types (matrix)
 		//compute graph from matrix
+		
+		DistanceMatrix matrix = computeDistanceMatrix(schema);
+	
+		//TODO in the original codebase this creates links (t1,s1) --s2-> (t2,s1*s2) which would imply this graph is labelled?
+		for(Type t1 : schema.getTypes()){
+			for(Type t2 : schema.getTypes()){
+				for(SelectivityClass sel1 : SelectivityClass.values()){
+					for(SelectivityClass sel2 : SelectivityClass.values()){
+						//if there exists a path of valid length from t1 to t2 with result selectivity class sel2
+						if(matrix.get(t1, t2).getOrDefault(sel2, Integer.MAX_VALUE) <= maxLength){
+							//add graph edge (t1, sel1) -> (t2, sel1 * sel2)
+							resolve(t1, sel1).addUniqueEdgeTo(resolve(t2, sel1.conjunction(sel2)));
+						}
+					}
+				}
+			}
+		}
+	
+	}
+	
+	private GraphNode<SelectivityType, Void> resolve(Type type, SelectivityClass sel){
+		return index.get(type).computeIfAbsent(sel, k->addUniqueNode(new SelectivityType(type, sel)));
 	}
 	
 	
@@ -23,9 +48,13 @@ public class SelectivityGraph{
 	
 	
 	
-	
-	
-	
+	/**
+	 * Computes the shortest path between any two nodes in
+	 * the schema graph. This is done using a modified version
+	 * of the Floyd-Warshall algorithm (all-pairs shortest path).
+	 * @param schema The schema to compute the distance matrix for.
+	 * @return The distance matrix for the given graph schema.
+	 */
 	private static DistanceMatrix computeDistanceMatrix(Schema schema){
 		int size = schema.getTypeCount();
 		int logSize = (int)Math.log(size) + 4;//TODO why does this use a natural log?
@@ -75,8 +104,8 @@ public class SelectivityGraph{
 		}
 		
 		private void overwrite(DistanceMatrix data){
-			for(int i = 0; i < size; i++){
-				for(int j = 0; j < size; j++){
+			for(int i = 0; i < getSize(); i++){
+				for(int j = 0; j < getSize(); j++){
 					Map<SelectivityClass, Integer> cell = get(i, j);
 					Map<SelectivityClass, Integer> other = data.get(i, j);
 					for(SelectivityClass sel : SelectivityClass.values()){

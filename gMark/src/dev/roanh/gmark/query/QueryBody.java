b149a2dea2388d4ee62e772ef7cae4b3926382b0
Roan Hofland <roan@roanh.dev>
2022-01-18 19:11:37 +0100

    Work on query body to SQL

--- gMark/src/dev/roanh/gmark/query/QueryBody.java
+++ gMark/src/dev/roanh/gmark/query/QueryBody.java
@@ -3,7 +3,9 @@ package dev.roanh.gmark.query;
 import java.util.List;
 import java.util.StringJoiner;
 
-public class QueryBody{
+import dev.roanh.gmark.output.SQL;
+
+public class QueryBody implements SQL{
 	private List<Conjunct> conjuncts;
 	
 	public QueryBody(List<Conjunct> conjuncts){
@@ -21,5 +23,31 @@ public class QueryBody{
 			joiner.add(conj.toString());
 		}
 		return joiner.toString();
+	}
+
+	@Override
+	public String toSQL(){
+		StringBuilder buffer = new StringBuilder();
+		int n = conjuncts.size();
+		
+		buffer.append("WITH RECURSIVE ");
+		for(int i = 0; i < n; i++){
+			buffer.append("c");
+			buffer.append(i);
+			buffer.append("(src, trg) AS ");
+			buffer.append(conjuncts.get(i).toSQL());
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+		return null;
 	}
 }

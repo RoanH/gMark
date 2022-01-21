6c4071098b1cdba2127896264aecf6f17b0ee2e6
Roan Hofland <roan@roanh.dev>
2022-01-21 02:47:36 +0100

    Start on more GUI tabs

---
+++ gMark/client/dev/roanh/gmark/client/QueryTab.java
@@ -0,0 +1,21 @@
+package dev.roanh.gmark.client;
+
+import javax.swing.JPanel;
+
+public class QueryTab extends JPanel{
+
+	public QueryTab(){
+		
+		
+		
+	}
+	
+	
+	public void openWorkload(){
+		
+		
+		
+		
+	}
+	
+}

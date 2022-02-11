b3b2632ee1c2651f98ff5a8d1a5ba3772a4849f5
Roan Hofland <roan@roanh.dev>
2022-02-11 21:44:25 +0100

    Complete the GenerationException javadoc

--- gMark/src/dev/roanh/gmark/exception/GenerationException.java
+++ gMark/src/dev/roanh/gmark/exception/GenerationException.java
@@ -11,10 +11,21 @@ public class GenerationException extends Exception{
 	 */
 	private static final long serialVersionUID = 4496821367499497928L;
 
+	/**
+	 * Constructs a new generation exception with
+	 * the given reason.
+	 * @param reason The exception reason.
+	 */
 	public GenerationException(String reason){
 		super(reason);
 	}
 	
+	/**
+	 * Constructs a new generation exception with
+	 * the given reason and cause.
+	 * @param reason The exception reason.
+	 * @param cause The original exception.
+	 */
 	public GenerationException(String reason, Throwable cause){
 		super(reason, cause);
 	}

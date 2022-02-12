7ef3860980f798ac114dbed2e3cf4bdea2b2dea0
Roan Hofland <roan@roanh.dev>
2022-02-12 01:50:54 +0100

    Move a lot of debugging logic to an example tab

---
+++ gMark/client/dev/roanh/gmark/client/ExampleTab.java
@@ -0,0 +1,65 @@
+package dev.roanh.gmark.client;
+
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import dev.roanh.gmark.ConfigParser;
+import dev.roanh.gmark.client.component.GraphPanel;
+import dev.roanh.gmark.core.Configuration;
+import dev.roanh.gmark.core.SelectivityClass;
+import dev.roanh.gmark.core.graph.Predicate;
+import dev.roanh.gmark.core.graph.Type;
+import dev.roanh.gmark.exception.ConfigException;
+import dev.roanh.gmark.exception.GenerationException;
+import dev.roanh.gmark.util.ConfigGraph;
+import dev.roanh.gmark.util.EdgeGraph;
+import dev.roanh.gmark.util.EdgeGraphData;
+import dev.roanh.gmark.util.SchemaGraph;
+import dev.roanh.gmark.util.SelectivityGraph;
+import dev.roanh.gmark.util.SelectivityType;
+
+/**
+ * Example tab that shows some of the important structures in gmark.
+ * @author Roan
+ * @see ConfigGraph
+ * @see SchemaGraph
+ * @see SelectivityGraph
+ * @see EdgeGraph
+ */
+public class ExampleTab extends JPanel{
+	/**
+	 * Serial ID.
+	 */
+	private static final long serialVersionUID = 8727642118457294081L;
+
+	/**
+	 * Constructs a new example tab.
+	 */
+	public ExampleTab(){
+		super(new BorderLayout());
+		this.setBorder(BorderFactory.createTitledBorder("Example"));
+
+		try{
+			Configuration config = ConfigParser.parse(ClassLoader.getSystemResourceAsStream("example.xml"));
+			SchemaGraph gs = new SchemaGraph(config.getSchema());
+			EdgeGraph eg = new EdgeGraph(gs, 3, SelectivityType.of(config.getTypes().get(2), SelectivityClass.EQUALS), SelectivityType.of(config.getTypes().get(1), SelectivityClass.CROSS), 2);
+			eg.removeNodeIf(n->n.getInEdges().size() + n.getOutEdges().size() == 0);
+			gs.removeUnreachable();
+
+			JTabbedPane tabs = new JTabbedPane();
+			tabs.addTab("Config Graph", new GraphPanel<Type, Predicate>(new ConfigGraph(config), Type::getAlias, Predicate::getAlias));
+			tabs.addTab("Schema Graph", new GraphPanel<SelectivityType, Predicate>(gs, SelectivityType::toString, Predicate::getAlias));
+			tabs.addTab("Selectivity Graph", new GraphPanel<SelectivityType, SelectivityClass>(new SelectivityGraph(config.getSchema(), 3), SelectivityType::toString, c->""));
+			tabs.addTab("Edge Graph", new GraphPanel<EdgeGraphData, Void>(eg));
+
+			this.add(new JLabel("On this tab we visually display some of the core structures used in gmark. You can drag graph nodes around with the mouse."), BorderLayout.PAGE_START);
+			this.add(tabs, BorderLayout.CENTER);
+		}catch(ConfigException | GenerationException e){
+			//cannot happen, this resource is internal and correct
+		}
+	}
+}
--- gMark/client/dev/roanh/gmark/client/GraphMark.java
+++ gMark/client/dev/roanh/gmark/client/GraphMark.java
@@ -5,75 +5,34 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
-import dev.roanh.gmark.ConfigParser;
 import dev.roanh.gmark.client.component.GraphPanel;
-import dev.roanh.gmark.core.Configuration;
 import dev.roanh.gmark.core.SelectivityClass;
 import dev.roanh.gmark.core.graph.Predicate;
-import dev.roanh.gmark.core.graph.Schema;
 import dev.roanh.gmark.core.graph.Type;
-import dev.roanh.gmark.exception.GenerationException;
-import dev.roanh.gmark.util.ConfigGraph;
-import dev.roanh.gmark.util.EdgeGraph;
-import dev.roanh.gmark.util.EdgeGraphData;
 import dev.roanh.gmark.util.Graph;
 import dev.roanh.gmark.util.Graph.GraphNode;
 import dev.roanh.util.Util;
-import dev.roanh.gmark.util.SchemaGraph;
-import dev.roanh.gmark.util.SelectivityGraph;
 import dev.roanh.gmark.util.SelectivityType;
 
 public class GraphMark{
 	private static final JFrame frame = new JFrame("gMark");
 
-	public static void main(String[] args) throws GenerationException{
+	public static void main(String[] args){
 		Util.installUI();
 		
-//		Configuration config = ConfigParser.parse(Paths.get("./test/test.xml"));
-//		//Configuration config = ConfigParser.parse(Paths.get("C:\\Users\\RoanH\\Downloads\\tmp\\gmark\\use-cases\\shop.xml"));
-//		Schema schema = config.getSchema();
-//		SchemaGraph gs = new SchemaGraph(config.getSchema());
-//		//paper 1=1
-//		//SelectivityType src = SelectivityType.of(config.getTypes().get(1), SelectivityClass.ONE_ONE);
-//		//conference 1<N
-//		//SelectivityType trg = SelectivityType.of(config.getTypes().get(3), SelectivityClass.ONE_N);
-//		//city 1<N
-//		//SelectivityType trg = SelectivityType.of(config.getTypes().get(4), SelectivityClass.ONE_N);
-//		//paper 1<N
-//		//SelectivityType trg = SelectivityType.of(config.getTypes().get(1), SelectivityClass.ONE_N);
-//		//journal 1=1
-//		//SelectivityType src = SelectivityType.of(config.getTypes().get(2), SelectivityClass.ONE_ONE);
-//		//journal N=N
-//		SelectivityType src = SelectivityType.of(config.getTypes().get(2), SelectivityClass.EQUALS);
-//		//paper 1x1
-//		//SelectivityType trg = SelectivityType.of(config.getTypes().get(1), SelectivityClass.CROSS);
-//		//paper NxN
-//		SelectivityType trg = SelectivityType.of(config.getTypes().get(1), SelectivityClass.CROSS);
-
-//		int maxLen = 3;
-//		
-//		EdgeGraph eg = new EdgeGraph(gs, maxLen, src, trg, 10);
-//		ConfigGraph cg = new ConfigGraph(config);
-//		SelectivityGraph sg = new SelectivityGraph(schema, maxLen);
-//		gs.removeUnreachable();
-//
 		JTabbedPane tabs = new JTabbedPane();
-//		tabs.addTab("Config Graph", new GraphPanel<Type, Predicate>(cg, Type::getAlias, Predicate::getAlias));
-//		tabs.addTab("Schema Graph", new GraphPanel<SelectivityType, Predicate>(gs, SelectivityType::toString, Predicate::getAlias));
-//		tabs.addTab("Edge Graph", new GraphPanel<EdgeGraphData, Void>(eg));
-//		tabs.addTab("Selectivity Graph", new GraphPanel<SelectivityType, SelectivityClass>(sg));
 //		tabs.addTab("Ref", buildRefGraph());
 //		tabs.addTab("Sel Ref", buildSelRefGraph());
 		tabs.addTab("Query Generation", new QueryTab());
+		tabs.addTab("Example", new ExampleTab());
 		
 		frame.add(tabs);
-		frame.setSize(800, 600);
+		frame.setSize(1000, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		
 //		triplesToSQL(
@@ -101,6 +60,7 @@ public class GraphMark{
 		}
 	}
 	
+	@Deprecated
 	private static GraphPanel<SelectivityType, Predicate> buildRefGraph(){
 		System.out.println("build ref graph");
 		Predicate a = new Predicate(0, "a", 0.0D);
@@ -138,6 +98,7 @@ public class GraphMark{
 		return p;
 	}
 	
+	@Deprecated
 	private static GraphPanel<SelectivityType, Void> buildSelRefGraph(){
 		System.out.println("build sel ref graph");
 		

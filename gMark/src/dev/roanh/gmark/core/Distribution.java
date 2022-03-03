fd298c902627e2ea8f3871cd59207533d220b99e
Roan Hofland <roan@roanh.dev>
2022-03-03 05:29:13 +0100

    Document the Distribution class

--- gMark/src/dev/roanh/gmark/core/Distribution.java
+++ gMark/src/dev/roanh/gmark/core/Distribution.java
@@ -2,11 +2,28 @@ package dev.roanh.gmark.core;
 
 import org.w3c.dom.Element;
 
+/**
+ * Abstract interface for probability distributions.
+ * @author Roan
+ */
 public abstract interface Distribution{
+	/**
+	 * Used when a distribution is not explicitly specified.
+	 */
 	public static final Distribution UNDEFINED = ()->DistributionType.UNDEFINED;
 
+	/**
+	 * Gets the type of this distribution.
+	 * @return The type of this distribution.
+	 */
 	public abstract DistributionType getType();
 	
+	/**
+	 * Parses the given configuration XML element
+	 * to the corresponding distribution.
+	 * @param data The XML element to parse.
+	 * @return The parsed distribution.
+	 */
 	public static Distribution fromXML(Element data){
 		return DistributionType.getByName(data.getAttribute("type")).newInstance(data);
 	}

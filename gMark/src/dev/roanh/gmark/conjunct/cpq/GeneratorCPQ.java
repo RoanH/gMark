a2b778611cff80b6770e9a1d3a90c65865cfbdc8
Roan Hofland <roan@roanh.dev>
2022-03-06 03:24:27 +0100

    Add a javadoc to the GeneratorCPQ class

--- gMark/src/dev/roanh/gmark/conjunct/cpq/GeneratorCPQ.java
+++ gMark/src/dev/roanh/gmark/conjunct/cpq/GeneratorCPQ.java
@@ -15,14 +15,31 @@ import dev.roanh.gmark.util.SelectivityGraph;
 import dev.roanh.gmark.util.SelectivityType;
 
 /**
- * Generator for CPQ (Conjunctive Path Queries).
+ * Generator for CPQs (Conjunctive Path Queries).
  * @author Roan
  */
 public class GeneratorCPQ implements ConjunctGenerator{
+	/**
+	 * The schema graph to use to generate CPQs.
+	 */
 	private SchemaGraph gs;
+	/**
+	 * The workload specifying what CPQs to generate.
+	 */
 	private WorkloadCPQ workload;
 	
-	public GeneratorCPQ(Workload wl){
+	/**
+	 * Constructs a new CPQ generator using the given workload.
+	 * @param wl The workload specification.
+	 * @throws IllegalArgumentException When the given workload
+	 *         specification is not of the type {@link WorkloadCPQ}.
+	 * @see WorkloadCPQ
+	 */
+	public GeneratorCPQ(Workload wl) throws IllegalArgumentException{
+		if(!(wl instanceof WorkloadCPQ)){
+			throw new IllegalArgumentException("Incorret workload type.");
+		}
+		
 		gs = new SchemaGraph(wl.getGraphSchema());
 		workload = (WorkloadCPQ)wl;
 	}

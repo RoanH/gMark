16a17546b1a6d128cd8624c1df35cdb54534f1d0
Roan Hofland <roan@roanh.dev>
2022-03-06 03:32:30 +0100

    Add a javadoc to the ConjunctCPQ class

--- gMark/src/dev/roanh/gmark/conjunct/cpq/ConjunctCPQ.java
+++ gMark/src/dev/roanh/gmark/conjunct/cpq/ConjunctCPQ.java
@@ -4,9 +4,22 @@ import dev.roanh.gmark.core.WorkloadType;
 import dev.roanh.gmark.query.Conjunct;
 import dev.roanh.gmark.util.IndentWriter;
 
+/**
+ * Defines a conjunct that has a CPQ as
+ * its inner query.
+ * @author Roan
+ * @see Conjunct
+ */
 public class ConjunctCPQ extends Conjunct{
+	/**
+	 * The CPQ in this conjunct.
+	 */
 	private CPQ cpq;
 	
+	/**
+	 * Constructs a new CPQ conjunct with the given CPQ.
+	 * @param cpq The CPQ for this conjunct.
+	 */
 	public ConjunctCPQ(CPQ cpq){
 		this.cpq = cpq;
 	}

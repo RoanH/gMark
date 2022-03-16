6613d15e1181e76de9492a69496f5b4f0653861c
Roan Hofland <roan@roanh.dev>
2022-03-16 02:05:43 +0100

    Document the Conjunct class

--- gMark/src/dev/roanh/gmark/query/Conjunct.java
+++ gMark/src/dev/roanh/gmark/query/Conjunct.java
@@ -4,37 +4,86 @@ import dev.roanh.gmark.core.WorkloadType;
 import dev.roanh.gmark.output.OutputXML;
 import dev.roanh.gmark.util.IndentWriter;
 
+/**
+ * Abstract base class for query conjuncts.
+ * @author Roan
+ */
 public abstract class Conjunct implements OutputXML{
+	/**
+	 * The source variable for this conjunct.
+	 */
 	private Variable source;
+	/**
+	 * The target variable for this conjunct.
+	 */
 	private Variable target;
+	/**
+	 * True if this conjunct has a Kleene star above it.
+	 */
 	private boolean star;
 	
+	/**
+	 * Constructs a new conjunct with the given source
+	 * variable, target variable and Kleene star status.
+	 * @param source The conjunct source variable.
+	 * @param target The conjunct target variable.
+	 * @param star True if the conjunct has a Kleene star above it.
+	 */
 	protected Conjunct(Variable source, Variable target, boolean star){
 		this.source = source;
 		this.target = target;
 		this.star = star;
 	}
 	
+	/**
+	 * Gets if this conjunct has a Kleene star above it.
+	 * @return True if this conjunct has a Kleene star above it.
+	 */
 	public boolean hasStar(){
 		return star;
 	}
 	
+	/**
+	 * Gets the source variable for this conjunct.
+	 * @return The source variable for this conjunct.
+	 */
 	public Variable getSource(){
 		return source;
 	}
 	
+	/**
+	 * Gets the target variable for this conjunct.
+	 * @return The target variable for this conjunct.
+	 */
 	public Variable getTarget(){
 		return target;
 	}
 	
-	//TODO should probably not be a thing
+	/**
+	 * Gets the string representation of the inner
+	 * part of this conjunct.
+	 * @return The inner string for this conjunct.
+	 */
 	protected abstract String getInnerString();
 	
-	//does not have to respect star
+	/**
+	 * Gets the SQL representation of the inner part
+	 * of this conjunct.
+	 * @return The SQL for the inner part of this conjunct.
+	 */
 	protected abstract String toPartialSQL();
 	
+	/**
+	 * Writes the XML representation of the inner part
+	 * of this conjunct to the given writer.
+	 * @param writer The writer to write to.
+	 */
 	protected abstract void writePartialXML(IndentWriter writer);
 	
+	/**
+	 * Gets the workload type this conjunct belongs to.
+	 * @return The workload type this conjunct belongs to.
+	 */
 	public abstract WorkloadType getType();
 	
 	@Override

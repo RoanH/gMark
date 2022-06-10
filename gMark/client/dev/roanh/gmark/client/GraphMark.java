05f16a47aa2860d6df25ff7612b558257a42b064
Roan Hofland <roan@roanh.dev>
2022-06-10 03:02:02 +0200

    Properly apply the GPL
    Doesn't functionally change anything, but this is properly in line with
    the instructions.
    
    Signed-off-by: Roan Hofland <roan@roanh.dev>

--- gMark/client/dev/roanh/gmark/client/GraphMark.java
+++ gMark/client/dev/roanh/gmark/client/GraphMark.java
@@ -1,3 +1,21 @@
+/*
+ * gMark: A domain- and query language-independent graph instance and query workload generator.
+ * Copyright (C) 2021  Roan Hofland (roan@roanh.dev).  All rights reserved.
+ * GitHub Repository: https://github.com/RoanH/gMark
+ *
+ * gMark is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gMark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package dev.roanh.gmark.client;
 
 import java.awt.BorderLayout;

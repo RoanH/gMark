0d6511c2b178176dbd062ae2876b0eda8deba2a4
Roan Hofland <roan@roanh.dev>
2022-06-18 21:50:17 +0200

    Fix typo

--- gMark/src/dev/roanh/gmark/util/Graph.java
+++ gMark/src/dev/roanh/gmark/util/Graph.java
@@ -269,7 +269,7 @@ public class Graph<V, E>{
 	 * Computes the adjacency list representation of this graph. For this the
 	 * unique ID of every graph node is used (see {@link GraphNode#getID()}.
 	 * The adjacency list is returned as a 2-dimensional array, the first dimension
-	 * has as many indices as there were nodes added graph. Each of these indices
+	 * has as many indices as there were nodes added to the graph. Each of these indices
 	 * corresponds to the unique ID of one of the nodes in the graph. Indices for
 	 * nodes no longer in the graph will have a value of <code>null</code>. All other
 	 * indices have an array with the IDs of all the nodes the node has an edge to.

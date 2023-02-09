b7b0e135abcde5c60722703dbe3a3aaed75d0bf2
Roan Hofland <roan.hofland@hotmail.com>
2023-02-09 12:07:15 +0900

    Matching unit testing

---
+++ gMark/test/dev/roanh/gmark/util/UtilTest.java
@@ -0,0 +1,48 @@
+/*
+ * gMark: A domain- and query language-independent graph instance and query workload generator.
+ * Copyright (C) 2021  Roan Hofland (roan@roanh.dev).  All rights reserved.
+ * GitHub Repository: https://github.com/RoanH/gMark
+ *
+ * gMark is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gMark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dev.roanh.gmark.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import dev.roanh.gmark.util.SimpleGraph.SimpleEdge;
+
+public class UtilTest{
+
+	@Test
+	public void matching0(){
+		SimpleGraph<String> g = new SimpleGraph<String>();
+		
+		g.addVertex("a");
+		g.addVertex("b");
+		g.addVertex("c");
+		g.addVertex("d");
+		
+		g.addEdge("a", "b");
+		g.addEdge("b", "c");
+		g.addEdge("c", "d");
+		g.addEdge("d", "a");
+		
+		List<SimpleEdge<String>> matching = Util.findMaximalMatching(g);
+		assertEquals(2, matching.size());
+	}
+}

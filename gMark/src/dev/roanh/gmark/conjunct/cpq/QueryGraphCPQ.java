2cde296c064cb4c475bdddef9355be072466e9e4
Roan Hofland <roan.hofland@hotmail.com>
2023-02-16 17:45:48 +0900

    Use the correct code block

--- gMark/src/dev/roanh/gmark/conjunct/cpq/QueryGraphCPQ.java
+++ gMark/src/dev/roanh/gmark/conjunct/cpq/QueryGraphCPQ.java
@@ -197,7 +197,7 @@ public class QueryGraphCPQ{
 	/**
 	 * Computes if there is a <b>query</b> homomorphism from this query graph <code>G</code>
 	 * to the given other graph <code>G'</code>. This implies that any edge traversal made in
-	 * this graph can be mimicked in the given other graph. Formally <code>G -> G'</code> or
+	 * this graph can be mimicked in the given other graph. Formally {@code G -> G'} or
 	 * <code>G</code> is contained in <code>G'</code> (as a subgraph).
 	 * <p>
 	 * Note: This method tests for <b>query</b> homomorphism, as such the identity of the source

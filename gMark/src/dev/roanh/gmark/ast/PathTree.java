23c0a19260d8adb694872f1a8ec97a937a723775
Roan Hofland <roan@roanh.dev>
2024-08-21 13:20:23 +0200

    AST to string conversion

--- gMark/src/dev/roanh/gmark/ast/PathTree.java
+++ gMark/src/dev/roanh/gmark/ast/PathTree.java
@@ -1,32 +1,31 @@
 package dev.roanh.gmark.ast;
 
 import dev.roanh.gmark.lang.QueryLanguageSyntax;
+import dev.roanh.gmark.util.IndentWriter;
 
 //fragment = atom for leaves
 public class PathTree<T extends QueryLanguageSyntax<T>>{
 	private final PathTree<T> left;
 	private final PathTree<T> right;
-//	private final OperationType operation;
 	private final T fragment;
 	
-	private PathTree(PathTree<T> left, PathTree<T> right, /*OperationType operation, */T fragment){
+	private PathTree(PathTree<T> left, PathTree<T> right, T fragment){
 		this.left = left;
 		this.right = right;
-//		this.operation = operation;
 		this.fragment = fragment;
 	}
 	
-	public boolean isLeaf(){//TODO maybe delegate to OP type
+	public boolean isLeaf(){
 		return left == null && right == null;
 	}
 	
-//	public boolean isUnary(){//TODO maybe delegate to OP type
-//		return left != null && right == null;
-//	}
-//	
-//	public boolean isBinary(){//TODO maybe delegate to OP type
-//		return left != null && right != null;
-//	}
+	public boolean isUnary(){
+		return getOperation().isUnary();
+	}
+	
+	public boolean isBinary(){
+		return getOperation().isBinary();
+	}
 	
 	public OperationType getOperation(){
 		return fragment.getOperationType();
@@ -36,12 +35,28 @@ public class PathTree<T extends QueryLanguageSyntax<T>>{
 		return fragment;
 	}
 	
+	protected void writeAST(IndentWriter writer){
+		if(isLeaf()){
+			writer.println(fragment.toString());
+		}else{
+			writer.println(getOperation().toString(), 2);
+			writer.print("- ");
+			left.writeAST(writer);
+			if(right != null){
+				writer.print("- ");
+				right.writeAST(writer);
+			}
+			
+			writer.decreaseIndent(2);
+		}
+	}
 	
-	
-	
-	
-	
-	
+	@Override
+	public String toString(){
+		IndentWriter writer = new IndentWriter();
+		writeAST(writer);
+		return writer.toString();
+	}
 	
 	public static <T extends QueryLanguageSyntax<T>> PathTree<T> ofAtom(T fragment){
 		return new PathTree<T>(null, null, fragment);

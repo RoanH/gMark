ee8af9f238b281b1bad23e3e3d62b2d53eb06393
Roan Hofland <roan@roanh.dev>
2024-10-05 18:02:33 +0200

    Provider documentation

--- QuickSilver/src/nl/group9/quicksilver/impl/Provider.java
+++ QuickSilver/src/nl/group9/quicksilver/impl/Provider.java
@@ -1,8 +1,16 @@
 package nl.group9.quicksilver.impl;
 
+import nl.group9.quicksilver.core.spec.DatabaseGraph;
 import nl.group9.quicksilver.core.spec.Evaluator;
 import nl.group9.quicksilver.core.spec.EvaluatorProvider;
 
+/**
+ * Simple bridging class to provided to concrete evaluator and
+ * database graph implementation that should be used by QuickSilver.
+ * @author Roan
+ * @see DatabaseGraph
+ * @see Evaluator
+ */
 public class Provider implements EvaluatorProvider<SimpleGraph, SimpleGraph>{
 
 	@Override

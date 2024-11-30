f8b8d4309396383383a2d34ddb99660e412b532b
Roan Hofland <roan@roanh.dev>
2024-11-30 18:18:00 +0100

    Document SLT

--- gMark/src/dev/roanh/gmark/eval/DatabaseGraph.java
+++ gMark/src/dev/roanh/gmark/eval/DatabaseGraph.java
@@ -11,29 +11,84 @@ import dev.roanh.gmark.util.graph.IntGraph;
 
 /**
  * Graph specification used for the graph representing the database data.
- * This interface is intentionally minimal and only specifies the methods
- * strictly required for the rest of the application to work.
  * <p>
- * Some other properties of the database graph:
+ * Some properties of the database graph:
  * <ul>
  * <li>Vertices, edges and label are represented using integers. This is
- * an abstractation away from the actual data, typically done for performance
- * reasons and for QuickSilver this conversion was done in advance for simplicity.</li>
+ * an abstraction away from the actual data, typically done for performance
+ * reasons and for this implementation the conversion was done in advance for simplicity.</li>
  * <li>The numerical vertex identifiers are consecutive, that is, if the database
  * graph has <i>V</i> vertices, then all vertices will have an identifier
  * <i>v</i> that satisfies {@code 0 <= v < V}.</li>
  * <li>The numerical label identifiers are consecutive, that is, if the database
  * graph has <i>L</i> labels, then all labels will have an identifier
  * <i>l</i> that satisfies {@code 0 <= l < L}.</li>
- * <li>In particular for real world data graph, there is no guarantee that no
+ * <li>In particular for real world data graphs, there is no guarantee that no
  * duplicates exist of some edges.</li>
  * </ul>
  * @author Roan
  */
 public class DatabaseGraph{
+	/**
+	 * The total number of vertices in this database graph. Note that not all vertices
+	 * need to be associated with an edge. Furthermore, vertices are identified by all the
+	 * integers in the interval [0, V).
+	 */
 	private final int vertexCount;
+	/**
+	 * Synopsis of paths of length 1 with a specific label. Effectively this is a
+	 * lookup map from label identifier to the total number of edges with that label
+	 * in the database graph. Concretely, {@code syn1[l]} contains the count of
+	 * edges in the database graph with label <code>l</code>.
+	 */
 	private final int[] syn1;
+	/**
+	 * Source-Label-Target (SLT) is a data structure that encodes all edges in the complete
+	 * database graph. The general design for this data structure is heavily inspired
+	 * by a compressed spare row matrix (CSR) and primarily intended to minimise CPU
+	 * cache misses. The SLT is represented using a single array, meaning it is designed
+	 * to be able to use a single contiguous block of memory.
+	 * <p>
+	 * The layout of this memory block is as follows:
+	 * <ol>
+	 * <li>Source Index: The start of the structure records at which offset in the SLT to
+	 * find the data for a specific node. For a node with some <code>id</code> the range
+	 * where information about that node is stored is between SLT index {@code SLT[id]}
+	 * (inclusive) and {@code SLT[id + 1]} (exclusive). If these two indices are identical
+	 * then a node does not have any outgoing edges. In this case no information is stored
+	 * for the node in question.
+	 * <li>Label Index: After resolving the start index for the data of a node we can read
+	 * the label index for that node. Similar to the source index, the label index records
+	 * for each label type where the target nodes are stored in the SLT. If no edges from
+	 * the source node exist with a given label, then these two indices are again equal and
+	 * no target nodes are stored.
+	 * <li>Target Range: The target range is the slice of the SLT referred to by the label
+	 * index which contains the node IDs of the actual targets for edges from the source
+	 * with the associated label.
+	 * </ol>
+	 * 
+	 * In general this means that if target nodes exist for some label we can find them in the
+	 * following slice of the SLT: {@code SLT[SLT[SLT[source] + label]...SLT[SLT[source] + label + 1]]}.
+	 * <p>
+	 * A small example graph with eight nodes, eight edges, two label types and the SLT
+	 * representing it can be seen in the figure below. Here the source index is shown in
+	 * red, the label index in blue, the target vertices associated with label 0 in purple
+	 * and the target vertices associated with label 1 in green.
+	 * 
+	 * <img alt="SLT Example" src="https://media.roanh.dev/gmark/SLT.svg" style="width: 100%;"/>
+	 * 
+	 * Assuming we want to find the data for node 4, then the data for this node is between index
+	 * {@code SLT[4] = 21} (inclusive) and {@code SLT[4 + 1] = 25} (exclusive). If we then
+	 * want to find the data for edges with label 0, this information is stored between index
+	 * {@code SLT[SLT[4] + 0] = 24} (inclusive) and {@code SLT[SLT[4] + 0 + 1] = 25} (exclusive). 
+	 */
 	private final int[] slt;
+	/**
+	 * The reverse SLT is analogous to the regular SLT, except the source index represents
+	 * target vertices and the edges are traversed in the reverse (target to source) direction.
+	 * In essence this is effectively a Target-Label-Source (TLS) index instead.
+	 * @see #slt SLT
+	 */
 	private final int[] reverseSlt;
 	
 	public DatabaseGraph(IntGraph graph){

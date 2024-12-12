6d4bb1d78ab965d75e280db49bd7ed780267048c
Roan Hofland <roan@roanh.dev>
2024-12-12 23:10:29 +0100

    Minor correction

--- gMark/src/dev/roanh/gmark/data/CardStat.java
+++ gMark/src/dev/roanh/gmark/data/CardStat.java
@@ -7,11 +7,11 @@ import dev.roanh.gmark.eval.PathQuery;
  * @author Roan
  * @param sources The number of distinct possible source nodes for the query, these are all
  *        potential start point nodes for a query. This will be 1 by definition
- *        if a query has a bound source node.
+ *        if a query has a bound source node (unless the query matches nothing).
  * @param paths The total number of distinct paths between the source and target nodes.
  * @param targets The number of distinct possible target nodes for the query, these are all
  *        potential end point nodes for a query. This will be 1 by definition
- *        if a query has a bound target node.
+ *        if a query has a bound target node (unless the query matches nothing).
  * @see PathQuery
  */
 public record CardStat(int sources, int paths, int targets){

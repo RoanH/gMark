7e23b19dee43b4fd7d2ec7f75438405b953c390d
Roan Hofland <roan@roanh.dev>
2025-06-15 18:06:02 +0200

    Document the CQ query graph implementation

--- gMark/src/dev/roanh/gmark/lang/cq/QueryGraphCQ.java
+++ gMark/src/dev/roanh/gmark/lang/cq/QueryGraphCQ.java
@@ -26,15 +26,36 @@ import dev.roanh.gmark.util.graph.generic.UniqueGraph;
 import dev.roanh.gmark.util.graph.generic.UniqueGraph.GraphEdge;
 import dev.roanh.gmark.util.graph.generic.UniqueGraph.GraphNode;
 
+/**
+ * Representation of the query graph of a CQ. This is effectively a visual representation
+ * of the CQ as a graph with the variables of the CQ corresponding to vertices and the formulae
+ * corresponding to directed edges in the graph.
+ * @author Roan
+ */
 public class QueryGraphCQ{
-	private final Set<VarCQ> variables;
+	/**
+	 * The set of vertices (variables) for the graph.
+	 */
+	private final Set<VarCQ> vertices;
+	/**
+	 * The set of edges (formulae) for the graph.
+	 */
 	private final List<AtomCQ> edges;
 	
-	public QueryGraphCQ(Set<VarCQ> variables, List<AtomCQ> edges){
-		this.variables = variables;
+	/**
+	 * Constructs a new CQ query graph with the given vertices and edges.
+	 * @param vertices The graph vertices (variables).
+	 * @param edges The graph edges (formulae).
+	 */
+	public QueryGraphCQ(Set<VarCQ> vertices, List<AtomCQ> edges){
+		this.vertices = vertices;
 		this.edges = edges;
 	}
 	
+	/**
+	 * Constructs a new CQ query graph from the given generic graph.
+	 * @param graph The graph structure to parse into a CQ query graph.
+	 */
 	public QueryGraphCQ(UniqueGraph<VarCQ, AtomCQ> graph){
 		this(
 			graph.getNodes().stream().map(GraphNode::getData).collect(Collectors.toSet()),
@@ -42,22 +63,38 @@ public class QueryGraphCQ{
 		);
 	}
 	
+	/**
+	 * Converts this CQ query graph to a CQ.
+	 * @return The constructed equivalent CQ.
+	 */
 	public CQ toCQ(){
-		return new CQ(variables, edges);
+		return new CQ(vertices, edges);
 	}
 	
+	/**
+	 * Gets the number of vertices (variables) in this query graph.
+	 * @return The number of vertices in this query graph.
+	 */
 	public int getVertexCount(){
-		return variables.size();
+		return vertices.size();
 	}
 	
+	/**
+	 * Gets the number of edges (formulae) in this query graph.
+	 * @return The number of edges in this query graph.
+	 */
 	public int getEdgeCount(){
 		return edges.size();
 	}
 	
+	/**
+	 * Converts this query graph to a generic graph instance with relevant metadata.
+	 * @return The constructed generic graph.
+	 */
 	public UniqueGraph<VarCQ, AtomCQ> toUniqueGraph(){
 		UniqueGraph<VarCQ, AtomCQ> graph = new UniqueGraph<VarCQ, AtomCQ>();
 		
-		for(VarCQ v : variables){
+		for(VarCQ v : vertices){
 			graph.addUniqueNode(v);
 		}
 		
